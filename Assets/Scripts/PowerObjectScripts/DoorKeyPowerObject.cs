59159cbfdbee36be92a6b5af4406830d7f1bc3d5
caleb parks <calebparks29@hotmail.com>
2016-12-23 13:29:18 -0600

    updated door key to use the powerObject class

---
+++ Assets/Scripts/PowerObjectScripts/DoorKeyPowerObject.cs
@@ -0,0 +1,156 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoorKeyPowerObject : PowerObject
+{
+
+    public PowerObject[] references;
+    LockedDoorPartPowerObject[] doorLocks;
+    int[] doorLocksRefsIDs;
+    LockedDoorController door;
+    Powerable powerable;
+    public bool unlocked = false;
+    public RoomLightsController roomLights;
+    public GameObject roomLightGameObject;
+    public override void Start()
+    {
+        powerType = PowerType.both;
+        doorLocks = new LockedDoorPartPowerObject[references.Length];
+        doorLocksRefsIDs = new int[references.Length];
+        doorLocks = GetComponentsInChildren<LockedDoorPartPowerObject>();
+        for (int i = 0; i < references.Length; i++)
+        {
+            if (references[i] != null)
+            {
+                doorLocks[i].UseLockPart(door);
+                doorLocks[i].reference = references[i];
+                PowerObject reference = references[i];
+                if (reference != null)
+                {
+                    doorLocksRefsIDs[i] = reference.GetInstanceID();
+                }
+                else
+                {
+                    doorLocksRefsIDs[i] = -1;
+                }
+                doorLocks[i].reference.sendReference(this);
+            }
+        }
+        if (roomLightGameObject != null)
+        {
+            roomLights = roomLightGameObject.GetComponent<RoomLightsController>();
+
+        }
+    }
+
+    // Update is called once per frame
+    //	void Update () {
+    //        if (roomLights != null)
+    //        {
+    //            if (unlocked && !roomLights.powered)
+    //            {
+    //                roomLights.powerOn();
+    //            } else if (!unlocked && roomLights.powered)
+    //            {
+    //                roomLights.powerOff();
+    //            }
+    //        }
+    //	}
+
+    public override void changePower(float[] powerArgs)
+    {
+        if (powerArgs.Length >= 2 && powerArgs[1] != -1)
+        {
+            int numOn = 0;
+            bool powered;
+            if (powerArgs[1] >= 1)
+            {
+                powered = true;
+            }
+            else
+            {
+                powered = false;
+            }
+            for (int i = 0; i < doorLocks.Length; i++)
+            {
+                if (powerArgs.Length >= 2 && doorLocksRefsIDs[i] == powerArgs[0])
+                {
+                    changeLockPowerState(i, powered);
+                }
+                if (doorLocks[i].isPowered)
+                {
+                    numOn++;
+                }
+            }
+            if (roomLights != null)
+            {
+                roomLights.changePower(new float[] { this.GetInstanceID(), (float)numOn / doorLocks.Length });
+            }
+            //			if (roomLights != null) {
+            //				roomLights.powerOff (numOn / doorLocks.Length);
+            //			}
+        }
+    }
+
+    public void changeLockPowerState(int index, bool state)
+    {
+        if (state)
+        {
+            doorLocks[index].turnOn();
+        }
+        else
+        {
+            doorLocks[index].turnOff();
+        }
+        checkIfUnlocked();
+        if (door != null)
+        {
+            door.changeLockPowerState(index, state);
+        }
+    }
+
+    void checkIfUnlocked()
+    {
+        int locksPowered = 0;
+        for (int i = 0; i < doorLocks.Length; i++)
+        {
+            if (doorLocks[i].isPowered)
+            {
+                locksPowered++;
+            }
+        }
+        if (locksPowered == doorLocks.Length && !unlocked)
+        {
+            unlocked = true;
+            if (powerable != null)
+            {
+                powerable.changePower(new float[] { GetInstanceID(), 1 });
+            }
+        }
+        else if (locksPowered != doorLocks.Length && unlocked)
+        {
+            unlocked = false;
+            if (powerable != null)
+            {
+                powerable.changePower(new float[] { GetInstanceID(), 0 });
+            }
+        }
+    }
+
+    public void sendReference(LockedDoorController reference)
+    {
+        door = reference;
+        //print(reference);
+    }
+
+    public void sendReference(Powerable reference)
+    {
+        powerable = reference;
+        //print(reference);
+    }
+
+    public GameObject getGameObject()
+    {
+        return gameObject;
+    }
+}
---
+++ Assets/Scripts/PowerObjectScripts/LockedDoorPartPowerObject.cs
@@ -0,0 +1,173 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class LockedDoorPartPowerObject : MonoBehaviour, Debugable
+{
+
+    public PowerObject reference;
+    bool powered;
+    public bool isPowered;
+    FadeEmission fadeEmission;
+    Coroutine coroutine;
+    LockedDoorController doorController;
+    [SerializeField]
+    public FadeDoorPartEmissionPowerObject fadePartEmission = new FadeDoorPartEmissionPowerObject();
+    public Renderer emission;
+    public Light pointLight;
+
+    void Awake()
+    {
+        coroutine = StartCoroutine(WaitForDoor());
+        fadePartEmission.doorPart = this;
+        fadePartEmission.color = emission.material.GetColor("_EmissionColor");
+        setColorAndIntensity(fadePartEmission.color, 0);
+    }
+
+    void Start()
+    {
+        setColorAndIntensity(fadePartEmission.color, 0);
+    }
+
+    void Update()
+    {
+        if (isPowered != powered)
+        {
+            if (isPowered)
+            {
+                turnOn();
+            }
+            else
+            {
+                turnOff();
+            }
+        }
+    }
+
+    public void turnOn()
+    {
+        isPowered = true;
+        powered = true;
+        fadePartEmission.turnOn();
+    }
+
+    public void turnOff()
+    {
+        isPowered = false;
+        powered = false;
+        fadePartEmission.turnOff();
+    }
+
+    IEnumerator WaitForDoor()
+    {
+        yield return null;
+        yield return null;
+        turnOn();
+    }
+
+    IEnumerator CheckForVariableChange()
+    {
+        while (Application.isPlaying)
+        {
+            if (isPowered != powered)
+            {
+                if (isPowered)
+                {
+                    turnOn();
+                }
+                else
+                {
+                    turnOff();
+                }
+            }
+            yield return new WaitForSeconds(0.5f);
+        }
+    }
+
+    public void UseLockPart(LockedDoorController door)
+    {
+        doorController = door;
+        StopCoroutine(coroutine);
+        if (Application.isEditor)
+        {
+            StartCoroutine(CheckForVariableChange());
+        }
+    }
+
+    public void debug()
+    {
+        doorController.toggleDoorLockState();
+    }
+
+    public void setColorAndIntensity(Color color, float intensity)
+    {
+        Color emissionColor = color * intensity; // Mathf.LinearToGammaSpace(intensity);
+        emission.material.SetColor("_EmissionColor", emissionColor);
+        DynamicGI.SetEmissive(emission, emissionColor);
+        if (pointLight != null)
+        {
+            pointLight.intensity = intensity;
+            pointLight.color = color;
+        }
+
+    }
+}
+
+[Serializable]
+public class FadeDoorPartEmissionPowerObject
+{
+    public LockedDoorPartPowerObject doorPart;
+    public Color color = Color.white;
+    Color currentColor = Color.white;
+    float currentIntensity = 0;
+    float minIntensity = 0;
+    public float maxIntensity = 1;
+    public float animationLength = 1;
+    Coroutine currentCoroutine;
+    public bool debug = false;
+
+
+    public void changeColorAndIntensity(float intensity, Color color)
+    {
+        currentIntensity = intensity;
+        currentColor = color;
+        if (debug)
+        {
+            Debug.Log(intensity);
+        }
+        doorPart.setColorAndIntensity(color, intensity);
+    }
+
+    public void changeColor(Color color)
+    {
+        changeColorAndIntensity(currentIntensity, color);
+    }
+
+    public void changeIntensity(float intensity)
+    {
+        changeColorAndIntensity(intensity, currentColor);
+    }
+
+    public void turnOn()
+    {
+        if (currentColor == Color.white)
+        {
+            currentColor = color;
+        }
+
+        if (currentCoroutine != null)
+        {
+            LerpCoroutine.stopCoroutine(currentCoroutine);
+        }
+        currentCoroutine = LerpCoroutine.LerpMinToMax(animationLength, minIntensity, maxIntensity, currentIntensity, changeIntensity, false);
+    }
+
+    public void turnOff()
+    {
+        if (currentCoroutine != null)
+        {
+            LerpCoroutine.stopCoroutine(currentCoroutine);
+        }
+        currentCoroutine = LerpCoroutine.LerpMinToMax(animationLength, minIntensity, maxIntensity, currentIntensity, changeIntensity, true);
+    }
+}
